41c9d063967d280cf51b8599feef11507ac8f3e3
Frank Zotynia <frankzotynia10@gmail.com>
2020-04-10 01:59:35 -0400

    Adjusted z probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "v1.6.5"
+#define CUSTOM_MACHINE_NAME "v1.6.6"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
@@ -961,7 +961,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, -5, -.270 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -5, -.290 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
