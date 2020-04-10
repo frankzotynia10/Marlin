19e984edb424d73b96a332bcf09e2157f2e3e363
Frank Zotynia <frankzotynia10@gmail.com>
2020-04-10 00:38:55 -0400

    Adjusted Probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,6 @@
 // Name displayed in the LCD "Ready" message and Info menu
 #define CUSTOM_MACHINE_NAME "v1.6.4t"
 
-
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
 //#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
@@ -986,7 +985,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 2
+#define MULTIPLE_PROBING 3
 //#define EXTRA_PROBING    1
 
 /**
