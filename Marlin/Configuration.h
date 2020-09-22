1f694b6dc8259d1c07e909fb911decee97613a6e
Frank Zotynia <frank@avnte.ch>
2020-09-22 02:06:58 -0400

    updated version numbe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "v1.6.11 updated"
+#define CUSTOM_MACHINE_NAME "v1.6.12 tram assist"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
